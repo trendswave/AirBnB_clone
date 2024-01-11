24a7f6f1202f97d2fa0fc9a5cdf27de8ed84b92e
Fortune-0 <fortunepeterspc07@gmail.com>
2024-01-11 11:38:30 +0100

    update of base_module

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+
+from base_module import BaseModel
+
+class User(BaseModel):
+    '''class to manage user object'''
+    first_name= ""
+    last_name = ""
+    email = ""
+    password = ""
+    
+            
