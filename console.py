1519d177eeef19dd515c40993bf998c62b696e4d
trendswave <paulchinedumicheal@gmail.com>
2024-01-12 16:05:43 +0100

    function that destroy an instance

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 import cmd
+from datetime import datetime
 from models.base_model import BaseModel
 from models import classes, storage
 import re
@@ -61,5 +62,22 @@ def do_show(self, arg):
             key = args[0] + "." + args[1]
             if key in storage.all():
                 print(storage.all()[key])
+            else:
+                print("** no instance found **")
+
+def do_destroy(self, arg):
+        'Deletes instance of <class> <id>'
+        args = parse(arg)
+        if not args or len(args) == 0 or args[0] == "":
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2 or args[1] == "":
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key in storage.all():
+                del storage.all()[key]
+                storage.save()
             else:
                 print("** no instance found **")
