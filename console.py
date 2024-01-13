c79abe628b513456a9ca31387b22c49efab6fecd
Fortune-0 <fortunepeterspc07@gmail.com>
2024-01-13 21:19:23 +0100

    cons

--- console.py
+++ console.py
@@ -5,25 +5,23 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    ''' 
-    '''
+    
+    '''class for command interpter '''
     prompt = "(hbnb)"
     
     
     def do_EOF(self, line):
-        """Handles End Of File character."""
+        
+        """ Handles End Of File character. """
         print()
         return True
     
-    def help_quit(self, line):
-        '''Quit command to exit the program'''
-        
-        # print("Quit command to exit the program")
-        return True
-    
     def do_quit(self, line):
+        
+        ''' Quit command to exit the program '''
         return True
     
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
