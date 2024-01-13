9d964979f406970d93c44b45ed08ac15de30ab49
trendswave <paulchinedumicheal@gmail.com>
2024-01-13 17:26:49 +0100

    test the quit method

--- test/test_console.py
+++ test/test_console.py
@@ -62,4 +62,9 @@ class TestConsole(unittest.TestCase):
             self.console.onecmd("\n")
             self.assertEqual('', f.getvalue())
     
-    
+     def test_quit(self):
+        """ Tests the quit method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            with self.assertRaises(SystemExit):
+                self.console.onecmd("quit")
+            self.assertEqual('', f.getvalue())
