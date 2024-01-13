5ee42ad75231d311545abc421dbf09da303cf77a
Fortune-0 <fortunepeterspc07@gmail.com>
2024-01-13 20:41:22 +0100

    testing console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,8 @@
 import json
 import os
-from models.base_module import BaseModel
-
-class FileStorage:
+from models.base_model import BaseModel
+from models.user import User
+class _FileStorage:
     '''Class for storing and reciving data'''
     __file_path = "file.json" 
     __objects = {}
@@ -11,25 +11,31 @@ class FileStorage:
         '''sets in __objects the obj with key <obj class name>.id"'''
         obj_cls_name = obj.__class__.__name__
         key = "{}.{}".format (obj_cls_name, obj.id)
-        FileStorage.__objects[key] = obj
+        _FileStorage.__objects[key] = obj
         
     def all(self):
         '''Retrives all the dictionaries. __object'''
-        return FileStorage.__objects
+        return _FileStorage.__objects
         
     def save(self):
         ''' Serilize the __object dic. to a json format'''
-        all_objs = FileStorage.__objects
+        all_objs = _FileStorage.__objects
         obj_dict = {}
         for obj in all_objs.keys():
             obj_dict[obj] = all_objs[obj].to_dict()
             
-        with open (FileStorage.__file_path, "w", encoding="utf-8") as file:
+        with open (_FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(obj_dict, file)
+            
+    def classes(self):
+        '''Returns a dictionary of valid classes and their reference'''
+        from models.user import User
+    
+        
     def reload(self):
         '''Deserilize the json file, to a python object'''
-        if os.path.isfile(FileStorage.__file.path):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
+        if os.path.isfile(_FileStorage.__file.path):
+            with open(_FileStorage.__file_path, 'r', encoding="utf-8") as file:
                 try:
                     obj_dict = json.load(file)
                     for key, value in obj_dict.items():
@@ -38,7 +44,7 @@ class FileStorage:
                         
                         instance = cls(**value)
                         
-                        FileStorage.__objects[key] = instance
+                        _FileStorage.__objects[key] = instance
                         
                 except Exception:
                     pass
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 
-from base_module import BaseModel
-
+# from base_model import BaseModel
+from base_model import BaseModel
 class User(BaseModel):
     '''class to manage user object'''
     first_name= ""
