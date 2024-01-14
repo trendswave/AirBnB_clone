24a7dd7492ab82d9376fa177db93e112314404da
trendswave <paulchinedumicheal@gmail.com>
2024-01-14 08:56:19 +0100

    updated the test console

--- test/test_console.py
+++ test/test_console.py
@@ -1,70 +1,99 @@
 #!/usr/bin/python3
-""" Tests console """
+
+"""Unnitest for console
+"""
+
+import sys
 import unittest
 from unittest.mock import patch
 from io import StringIO
-import pep8
-import os
-import json
-import console
-import tests
 from console import HBNBCommand
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.engine.file_storage import FileStorage
+from models import storage
+
+
 class TestConsole(unittest.TestCase):
-    """ Tests console """
 
     @classmethod
     def setUpClass(cls):
-        """ Creates an instance """
         cls.console = HBNBCommand()
-    
 
-    @classmethod
-    def teardown(cls):
-        """ Deletes the instance """
-        del cls.console
+    def setUp(self):
+        self.console.preloop()
 
     def tearDown(self):
-        """ Removes the JSON file """
-        try:
-            os.remove("file.json")
-        except:
-            pass
+        self.console.postloop()
 
-    def test_pep8_console(self):
-        """ Tests this script for PEP8 styling """
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(["console.py"])
-        self.assertEqual(p.total_errors, 0, 'PEP8 Failed') 
-    
-    def test_docstrings(self):
-        """ Checks methods for docstrings """
-        self.assertIsNotNone(console.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
-        self.assertIsNotNone(HBNBCommand.default.__doc__)
+    @patch('sys.stdout', new_callable=StringIO)
+    def assert_stdout(self, expected_output, mock_stdout, command):
+        self.console.onecmd(command)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_empty(self):
-        """ Tests the emptyline method """
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("\n")
-            self.assertEqual('', f.getvalue())
-    
-    def test_quit(self):
-        """ Tests the quit method """
-        with patch('sys.stdout', new=StringIO()) as f:
-            with self.assertRaises(SystemExit):
-                self.console.onecmd("quit")
-            self.assertEqual('', f.getvalue())
+    def test_create_show_destroy_all_update_commands(self):
+        storage.reset()
+
+        # Create
+        expected_output = "f1r57-1n574nc3\n"
+        self.assert_stdout(expected_output, "create BaseModel")
+        obj = storage.all()['BaseModel.f1r57-1n574nc3']
+
+        # Show
+        expected_output = str(obj) + '\n'
+        self.assert_stdout(expected_output, "show BaseModel {}".format(obj.id))
+
+        # Destroy
+        self.assert_stdout("", "destroy BaseModel {}".format(obj.id))
+        self.assertNotIn(obj, storage.all().values())
+
+        # All
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        expected_output = "[{}, {}]\n".format(str(obj1), str(obj2))
+        self.assert_stdout(expected_output, "all BaseModel")
+
+        # Update
+        self.assert_stdout(
+            "",
+            'update BaseModel {} name "New Name"'.format(obj1.id)
+        )
+        updated_obj = storage.all()['BaseModel.' + obj1.id]
+        self.assertEqual(updated_obj.name, "New Name")
+
+    def test_invalid_commands(self):
+        storage.reset()
+
+        # Invalid create command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "create InvalidClass")
+
+        # Invalid show command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "show InvalidClass")
+
+        # Invalid destroy command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "destroy InvalidClass")
+
+        # Invalid all command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "all InvalidClass")
+
+        # Invalid update command
+        expected_output = "** class doesn't exist **\n"
+        self.assert_stdout(expected_output, "update InvalidClass")
+
+    def test_empty_line_quit_commands(self):
+        # Empty line
+        expected_output = ""
+        self.assert_stdout(expected_output, "")
+
+        # Quit command
+        self.assertTrue(self.console.onecmd("quit"))
+
+    def test_help_commands(self):
+        expected_output = "Quit command to exit the program\n"
+        self.assert_stdout(expected_output, "help quit")
+
+
+if __name__ == '__main__':
+    unittest.main()
