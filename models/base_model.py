60757867f82883e6503d316b02fd2ca9a3d05a74
fortune <fortunepeterspc07@gmail.com>
2024-01-15 19:47:47 -0800

    upadte

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,8 @@
 
 import uuid
 from datetime import datetime
-# import models
+import models
+from models import storage
 
 
 class BaseModel:
@@ -33,7 +34,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            # models.storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -57,3 +58,18 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".
+            format(key, type(my_model_json[key]), my_model_json[key]))
