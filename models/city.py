aa50f57faddfe10b4e6ab15fb86fa9695ec45c7f
fortune <fortunepeterspc07@gmail.com>
2024-01-21 10:31:37 -0800

    out

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from base_model import BaseModel
-
-class User(BaseModel):
-    
-    '''This collect the city the user is in'''  
-    city: str = ""
-    state_id: str = ""
--- models/state.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from base_model import BaseModel
-
-class User(BaseModel):
-    """Manages the State of the user. It inherits from BaseModel."""
-    name: str = ""
